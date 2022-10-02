6cb309a0f40bfff22e695afd03f6877e4818a781
pppiyo <yueqinli@usc.edu>
2022-10-02 16:04:16 -0700

    dynamically generate new sections. tbd. optional.

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,7 @@
 var lng, lat, formData;
 
 window.addEventListener('load', event => {
-  $("#submit-button").click(function () {
+  $("#submit").click(function () {
     if (!$("#keyword")[0].checkValidity())
       $("#keyword")[0].reportValidity()
     else if (!$("#category")[0].checkValidity())
@@ -11,12 +11,33 @@ window.addEventListener('load', event => {
     else
       submitForm();
   });
+
+  $("#clear").click(function () {
+    document.getElementById("form").reset();
+    document.getElementById("table").innerHTML = ``;
+    document.getElementById("details").innerHTML = ``;
+    // removeChildren(document.getElementsByTagName("body"));
+  })
+
+
 });
+
+// function removeChildren(parent) {
+
+//   console.log(kids);
+//   var kids = parent.childNodes; // Get the list of children 
+//   console.log(kids);
+  // var numkids = kids.childElementCount; // Figure out how many children there are 
+  // console.log(numkids);
+  // for (var i = 1; i <= numkids; i++) { // Loop backward through the children 
+    // parent.removeChild(kids[i]); // Remove a child 
+  // }
+  
+// }
 
 
 function submitForm() {
   event.preventDefault();  // FOR DEBUG PURPOSE
-
   
   formData = new FormData(form); // formData global variable
   // formData.json()
@@ -25,7 +46,7 @@ function submitForm() {
   formData.forEach((value, key) => object[key] = value);
   var jsonFormData = JSON.stringify(object);
 
-  document.getElementById("output").innerHTML = `${jsonFormData}`;
+  // document.getElementById("table").innerHTML = `${jsonFormData}`;
   
   var checkBox = document.getElementById("detect-location");
   
@@ -47,15 +68,14 @@ function submitForm() {
     } else {
 
       str = str.split(',').join("+");
-      // document.getElementById("output").innerHTML = `${str}`;
+      // document.getElementById("table").innerHTML = `${str}`;
       useGeoCoding(str);
 
     }
 
   }
-  
-  do_ajax(jsonFormData);
 
+  do_ajax(jsonFormData);
 }
 
 
@@ -81,11 +101,11 @@ function useGeoCoding(location) {
   xhr.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
       var json = xhr.response;
-      // document.getElementById("output").innerHTML = `${json}`; // FOR DEBUG PURPOSE
+      // document.getElementById("table").innerHTML = `${json}`; // FOR DEBUG PURPOSE
       // console.log(json); // FOR DEBUG PURPOSE
       var obj = JSON.parse(json);
       if (obj["status"] == "ZERO_RESULTS") {
-        alert("recheck");
+        alert("No results");
       } else {
         lat = obj["results"]["0"]["geometry"]["location"]["lat"];
         lng = obj["results"]["0"]["geometry"]["location"]["lng"];
@@ -117,12 +137,16 @@ function enableLocationBox() {
 
 function do_ajax(jsonFormData) {
   var req = new XMLHttpRequest();
-  var output = document.getElementById('output');
+  var table = document.getElementById('table');
   req.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
-      output.innerHTML = this.responseText;
+      // var tableSection = document.createElement('section');
+      // tableSection.innerHTML = `<div id="table">${this.responseText}</div>`;
+      // document.body.appendChild(tableSection);
+
+      table.innerHTML = this.responseText;
     } else {
-      output.innerHTML = "hi thehihidfidhs ..."; // FOR DEBUG PURPOSE
+      table.innerHTML = "hi thehihidfidhs ..."; // FOR DEBUG PURPOSE
     }
   }
 
